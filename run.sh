ba98add902e60cbff97b50b6a75e475e5dabe925
softnetix <46715846+softnetix@users.noreply.github.com>
2024-02-21 13:08:51 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,41 @@
+# Launch Kafka Connect
+/etc/confluent/docker/run &
+#
+# Wait for Kafka Connect listener
+echo "Waiting for Kafka Connect Worker to start listening on localhost ⏳"
+while : ; do
+  curl_status=$(curl -s -o /dev/null -w %{http_code} http://localhost:8084/connectors)
+  echo -e $(date) " Kafka Connect listener HTTP state: " $curl_status " (waiting for 200)"
+  if [ $curl_status -eq 200 ] ; then
+    break
+  fi
+  sleep 5
+done
+
+CLICKHOUSE_SINK_CONNECTOR_NAME=clickhouse-sink-connector
+
+CLICKHOUSE_SINK_CONNECTOR_CONFIG="{
+           \"connector.class\": \"com.clickhouse.kafka.connect.ClickHouseSinkConnector\",
+           \"name\": \"${CLICKHOUSE_SINK_CONNECTOR_NAME}\",
+           \"tasks.max\": 20,
+           \"topics\": \"player_bet_transaction_details\",
+           \"ssl\": false,
+           \"hostname\": \"${CLICKHOUSE_HOSTNAME}\",
+           \"database\": \"${CLICKHOUSE_DATABASE}\",
+           \"username\": \"${CLICKHOUSE_USERNAME}\",
+           \"password\": \"${CLICKHOUSE_PASSWORD}\",
+           \"port\": \"${CLICKHOUSE_PORT}\",
+           \"key.converter.schemas.enable\": false,
+           \"key.converter\": \"org.apache.kafka.connect.storage.StringConverter\",
+           \"value.converter.schemas.enable\": false,
+           \"value.converter\": \"org.apache.kafka.connect.json.JsonConverter\",
+           \"exactlyOnce\": true,
+           \"schemas.enable\": false,
+           \"errors.tolerance\": \"all\",
+           \"tableRefreshInterval\": 600
+       }"
+
+echo -e "\n--\n+> Starting to configure ClickHouse Sink Connector"
+# shellcheck disable=SC2090
+curl -s -X PUT -H  "Content-Type:application/json" http://localhost:8084/connectors/${CLICKHOUSE_SINK_CONNECTOR_NAME}/config -d "${CLICKHOUSE_SINK_CONNECTOR_CONFIG}"
+sleep infinity
