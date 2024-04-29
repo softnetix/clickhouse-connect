01685289cc5ba538b0c462fd16237a5045f703e1
Artsiom Savinko <artsiom.savinko@softnetix.io>
2024-04-29 16:43:33 +0300

    refactor config

--- run.sh
+++ run.sh
@@ -12,39 +12,51 @@ while : ; do
   sleep 5
 done
 
-CLICKHOUSE_SINK_CONNECTOR_CONFIG="{
-           \"connector.class\": \"com.clickhouse.kafka.connect.ClickHouseSinkConnector\",
-           \"name\": \"${CLICKHOUSE_SINK_CONNECTOR_NAME}\",
-           \"tasks.max\": 10,
-           \"topics\": \"DATA_WAREHOUSE__PLAYER_BET_TRANSACTION_DETAILS,DATA_WAREHOUSE__PLAYER,DATA_WAREHOUSE__PLAYER_DETAILS,DATA_WAREHOUSE__PLAYER_AFFILIATE,DATA_WAREHOUSE__PLAYER_LINKED_AFFILIATE,DATA_WAREHOUSE__PLAYER_BONUS\",
-           \"ssl\": false,
-           \"hostname\": \"${CLICKHOUSE_HOSTNAME}\",
-           \"database\": \"${CLICKHOUSE_DATABASE}\",
-           \"username\": \"${CLICKHOUSE_USERNAME}\",
-           \"password\": \"${CLICKHOUSE_PASSWORD}\",
-           \"port\": \"${CLICKHOUSE_PORT}\",
-           \"key.converter.schemas.enable\": false,
-           \"worker.sync.timeout.ms\": 30000,
-           \"key.converter\": \"org.apache.kafka.connect.storage.StringConverter\",
-           \"value.converter.schemas.enable\": false,
-           \"value.converter\": \"org.apache.kafka.connect.json.JsonConverter\",
-           \"exactlyOnce\": false,
-           \"schemas.enable\": false,
-           \"errors.tolerance\": \"all\",
-           \"tableRefreshInterval\": 6,
-           \"consumer.override.fetch.min.bytes\": 10485760,
-           \"consumer.override.fetch.max.bytes\": 52428800,
-           \"consumer.override.fetch.max.wait.ms\": 5000,
-           \"consumer.override.max.poll.records\": 5000,
-           \"consumer.override.max.partition.fetch.bytes\": 52428800,
-           \"consumer.override.enable.auto.commit\": false,
-           \"jdbcConnectionProperties\": \"?socket_timeout=30000\",
-           \"topic2TableMap\": \"${ENVIRONMENT_CODE}__DATA_WAREHOUSE__PLAYER_BET_TRANSACTION_DETAILS=player_bet_transaction_details_distributed,${ENVIRONMENT_CODE}__DATA_WAREHOUSE__PLAYER=player_distributed,${ENVIRONMENT_CODE}__DATA_WAREHOUSE__PLAYER_DETAILS=player_details_distributed,${ENVIRONMENT_CODE}__DATA_WAREHOUSE__PLAYER_AFFILIATE=player_affiliate_distributed,${ENVIRONMENT_CODE}__DATA_WAREHOUSE__PLAYER_LINKED_AFFILIATE=player_linked_affiliate_distributed,${ENVIRONMENT_CODE}__DATA_WAREHOUSE__PLAYER_BONUS=player_bonus_distributed\",
-           \"transforms\": \"AddPrefix\",
-           \"transforms.AddPrefix.type\": \"org.apache.kafka.connect.transforms.RegexRouter\",
-           \"transforms.AddPrefix.regex\": \".*\",
-           \"transforms.AddPrefix.replacement\": \"${ENVIRONMENT_CODE}__\$0\"
-       }"
+topics=$(echo "$TOPIC2TABLEMAP" | awk -F ',' '{for (i=1; i<=NF; i++) print $i}' | awk -F '=' '{print $1}' | tr '\n' ',' | sed 's/,$//')
+
+map=$(echo "$TOPIC2TABLEMAP" | awk -F ',' -v env_code="$ENVIRONMENT_CODE" '{for (i=1; i<=NF; i++) print env_code "__"$i}' | tr '\n' ',' | sed 's/,$//')
+
+CLICKHOUSE_SINK_CONNECTOR_CONFIG=$(cat <<EOF
+{
+  "connector.class": "com.clickhouse.kafka.connect.ClickHouseSinkConnector",
+  "name": "${CLICKHOUSE_SINK_CONNECTOR_NAME}",
+  "tasks.max": 10,
+  "topics": "$topics",
+  "ssl": false,
+  "hostname": "${CLICKHOUSE_HOSTNAME}",
+  "database": "${CLICKHOUSE_DATABASE}",
+  "username": "${CLICKHOUSE_USERNAME}",
+  "password": "${CLICKHOUSE_PASSWORD}",
+  "port": ${CLICKHOUSE_PORT},
+  "worker.sync.timeout.ms": 30000,
+  "key.converter.schemas.enable": ${CONNECT_KEY_CONVERTER_SCHEMAS_ENABLE},
+  "key.converter": "${CONNECT_KEY_CONVERTER}",
+  "value.converter.schemas.enable": ${CONNECT_VALUE_CONVERTER_SCHEMAS_ENABLE},
+  "value.converter": "${CONNECT_VALUE_CONVERTER}",
+  "exactlyOnce": ${CONNECT_SINK_EXACTLY_ONCE},
+  "schemas.enable": false,
+  "errors.tolerance": "${CONNECT_ERRORS_TOLERANCE}",
+  "errors.log.enable": ${CONNECT_ERRORS_LOG_ENABLE},
+  "errors.log.include.messages": ${CONNECT_ERRORS_LOG_INCLUDE_MESSAGES},
+  "errors.deadletterqueue.topic.name": "${CONNECT_ERRORS_DEADLETTERQUEUE_TOPIC_NAME}",
+  "errors.deadletterqueue.topic.replication.factor": ${CONNECT_ERRORS_DEADLETTERQUEUE_TOPIC_REPLICATION_FACTOR},
+  "errors.deadletterqueue.context.headers.enable": ${CONNECT_ERRORS_DEADLETTERQUEUE_CONTEXT_HEADERS_ENABLE},
+  "tableRefreshInterval": 6,
+  "consumer.override.fetch.min.bytes": ${CONSUMER_OVERRIDE_FETCH_MIN_BYTES},
+  "consumer.override.fetch.max.bytes": ${CONSUMER_OVERRIDE_FETCH_MAX_BYTES},
+  "consumer.override.fetch.max.wait.ms": ${CONSUMER_OVERRIDE_FETCH_MAX_WAIT_MS},
+  "consumer.override.max.poll.records": ${CONSUMER_OVERRIDE_MAX_POLL_RECORDS},
+  "consumer.override.max.partition.fetch.bytes": ${CONSUMER_OVERRIDE_MAX_PARTITION_FETCH_BYTES},
+  "consumer.override.enable.auto.commit": ${CONSUMER_OVERRIDE_ENABLE_AUTO_COMMIT},
+  "jdbcConnectionProperties": "?socket_timeout=30000",
+  "topic2TableMap": "$map",
+  "transforms": "AddPrefix",
+  "transforms.AddPrefix.type": "org.apache.kafka.connect.transforms.RegexRouter",
+  "transforms.AddPrefix.regex": ".*",
+  "transforms.AddPrefix.replacement": "${ENVIRONMENT_CODE}__\$0"
+}
+EOF
+)
 
 echo -e "\n--\n+> Starting to configure ClickHouse Sink Connector"
 # shellcheck disable=SC2090
